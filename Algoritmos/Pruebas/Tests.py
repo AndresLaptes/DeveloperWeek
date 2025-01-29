3ec92bbcddf596e1a34ef4fa197c902ce02800c8
Andres Lucian Laptes Costan <alaptes.210404@gmail.com>
2025-01-29 21:16:15 +0100

    Vamos scoreHack

--- Algoritmos/Pruebas/Tests.py
+++ Algoritmos/Pruebas/Tests.py
@@ -84,12 +84,99 @@ def evaluar_AñoEscolar(team, datos) -> float:
 
                 diferencia_year = abs(YEAR_TO_INDEX[year1] - YEAR_TO_INDEX[year2])
 
-                score += 1 - (np.log(1 + diferencia_year) / np.log(max_year - min_year + 1))
+                score += 1.0 - (diferencia_year / max_year)
 
                 num_comparaciones += 1
 
         # Devolver el puntaje normalizado
-        return score / num_comparaciones
+        return score / num_comparaciones if num_comparaciones > 0 else 1.0
+
+def evaluar_Intereses(team, datos) -> float:
+    if len(team) == 1:
+        return 1.0
+
+    score = 0
+    total_intereses = 0
+
+    for i in range(len(team)):
+        integrante1 = team[i]
+        for j in range(i + 1, len(team)):
+            integrante2 = team[j]
+
+            intereses1 = datos.loc[datos['ID'] == integrante1['ID'], 'Interests'].values[0]
+            intereses2 = datos.loc[datos['ID'] == integrante2['ID'], 'Interests'].values[0]
+
+            intereses1 = set(intereses1.split(", "))
+            intereses2 = set(intereses2.split(", "))
+
+            if not len(intereses1.union(intereses2)) == 0:
+                score += len(intereses1.intersection(intereses2))/len(intereses1.union(intereses2))
+            
+            total_intereses += 1
+            
+
+    # Devolver el puntaje normalizado
+    return score / total_intereses if total_intereses > 0 else 1.0
+
+
+INVALID_PREFERENCIA = set(["Don't know", "Don't care"])
+
+def evaluar_Preferencia(team, datos) -> float:
+    score = 0
+
+    if len(team) == 1:
+        return 1.0
+    
+    normalizar = 0
+
+    for i in range(len(team)):
+        integrante1 = team[i]
+        for j in range(i + 1, len(team)):
+            integrante2 = team[j]
+
+            preferencia1 = datos.loc[datos['ID'] == integrante1['ID'], 'Preferred Role'].values[0]
+            preferencia2 = datos.loc[datos['ID'] == integrante2['ID'], 'Preferred Role'].values[0]
+
+            if preferencia1 not in INVALID_PREFERENCIA and preferencia2 not in INVALID_PREFERENCIA:
+                if preferencia1 != preferencia2:
+                    score += 1
+                normalizar += 1
+
+    return score / normalizar if normalizar > 0 else 1.0
+
+def evaluar_Experiencia(team, datos) -> float:
+    score = 0
+
+    if len(team) == 1:
+        return 1.0
+    
+    normalizar = 0
+
+    for i in range(len(team)):
+        integrante1 = team[i]
+        for j in range(i + 1, len(team)):
+            integrante2 = team[j]
+
+            experiencia1 = datos.loc[datos['ID'] == integrante1['ID'], 'Experience Level'].values[0]
+            experiencia2 = datos.loc[datos['ID'] == integrante2['ID'], 'Experience Level'].values[0]
+
+            if experiencia1 != experiencia2:
+                score += 1
+            
+            normalizar += 1
+
+    return score/normalizar
+
+def evaluar_Hackathons(team, datos) -> float:
+    score = 0
+
+    max_hackathons = datos['Hackathons Done'].max()
+    
+    for integrante in team:
+        hackathons = datos.loc[datos['ID'] == integrante['ID'], 'Hackathons Done'].values[0]
+        score += hackathons
+
+    return score/ (max_hackathons * len(team))
 
 try:
     datos = pd.read_csv(get_path_csv("Data"))
@@ -98,14 +185,23 @@ try:
     for i in range(len(temas)):
         for j in range(len(temas[i])):
             print(f"Edad de {temas[i][j]['Name']}: {temas[i][j]['Age']}, ")
-            print(f"año escolar: {temas[i][j]['Year of Study']}")
+            print(f"año escolar: {temas[i][j]['Year of Study']}, ")
+            print(f"interese: {temas[i][j]['Interests']}")
+            print(f"preferencia: {temas[i][j]['Preferred Role']}")
+            print(f"experiencia: {temas[i][j]['Experience Level']}")
+            print(f"Hackathons: {temas[i][j]['Hackathons Done']}")
         print(f"Score Edad: {evaluar_Edad(temas[i], datos)}")
-        print(f"Score Año: {evaluar_AñoEscolar(temas[i], datos)}\n")
+        print(f"Score Año: {evaluar_AñoEscolar(temas[i], datos)}")
+        print(f"Score Intereses: {evaluar_Intereses(temas[i], datos)}")
+        print(f"Score Preferencia: {evaluar_Preferencia(temas[i], datos)}")
+        print(f"Score Experiencia: {evaluar_Experiencia(temas[i], datos)}")
+        print(f"Score Hackathons: {evaluar_Hackathons(temas[i], datos)}\n")
     
     print("\n")
 
-
-    print(set(datos['University']))
+    preferencias = set(datos["Hackathons Done"])
+    print(preferencias)
+    
 
 
 except Exception as e:
--- Algoritmos/main.py
+++ Algoritmos/main.py
@@ -47,6 +47,8 @@ def generateRandomTeams(datos):
 MAX_NUM_PARTNERS = 4
 YEARS = ['1st year', '2nd year', '3rd year', '4th year', 'Masters', 'PhD']
 YEAR_TO_INDEX = {year: index for index, year in enumerate(YEARS)}
+INVALID_PREFERENCIA = set(["Don't know", "Don't care"])
+
 
 class TeamFormation:
     # Equipos -> todos los equipos disponibles, 1 equipo es una lista de los id de los participantes (lsit(list(String)))
@@ -87,7 +89,7 @@ class TeamFormation:
         except Exception as e:
             print("Error al procesar el archivo:", e)
 
-
+    # Función que crea a un individuo
     def crearIndividuo(self) -> tuple:
         creado_por_usuario = True
 
@@ -117,8 +119,8 @@ class TeamFormation:
         edad_max = self.Participantes['Age'].max()
 
         for i in range(len(team)):
+            integrante1 = team[i]
             for j in range(i + 1, len(team)):
-                integrante1 = team[i]
                 integrante2 = team[j]
 
                 edad1 = self.Participantes.loc[self.Participantes['ID'] == integrante1, 'Age'].values[0]
@@ -132,7 +134,7 @@ class TeamFormation:
                 num_comparaciones += 1
 
         # Devolver el puntaje normalizado 
-        return score/num_comparaciones
+        return score/num_comparaciones if num_comparaciones > 0 else 1.0
 
     def evaluar_AñoEscolar(self, team) -> float:
         if len(team) == 1:
@@ -145,8 +147,8 @@ class TeamFormation:
         max_year = len(YEARS) - 1
 
         for i in range(len(team)):
+            integrante1 = team[i]
             for j in range(i + 1, len(team)):
-                integrante1 = team[i]
                 integrante2 = team[j]
 
                 year1 = self.Participantes.loc[self.Participantes['ID'] == integrante1, 'Year'].values[0]
@@ -154,13 +156,42 @@ class TeamFormation:
 
                 diferencia_year = abs(YEAR_TO_INDEX[year1] - YEAR_TO_INDEX[year2])
 
-                # score calculado en logaritmo para penalizar diferencias grandes
-                score += 1 - (np.log(1 + diferencia_year) / np.log(max_year - min_year + 1))
+                score += 1.0 - (diferencia_year / max_year)
 
                 num_comparaciones += 1
 
-        return score / num_comparaciones
+        return score / num_comparaciones if num_comparaciones > 0 else 1.0
 
+
+    # Calcula las puntuaciones normalizadas de los intereses entre los Participantes
+    def evaluar_Intereses(self, team) -> float:
+        if len(team) == 1:
+            return 1.0
+
+        score = 0
+        total_intereses = 0
+
+        for i in range(len(team)):
+            integrante1 = team[i]
+            for j in range(i + 1, len(team)):
+                integrante2 = team[j]
+
+                intereses1 = self.Participantes.loc[self.Participantes['ID'] == integrante1['ID'], 'Interests'].values[0]
+                intereses2 = self.Participantes.loc[self.Participantes['ID'] == integrante2['ID'], 'Interests'].values[0]
+
+                intereses1 = set(intereses1.split(", "))
+                intereses2 = set(intereses2.split(", "))
+
+                if not len(intereses1.union(intereses2)) == 0:
+                    score += len(intereses1.intersection(intereses2))/len(intereses1.union(intereses2))
+            
+                total_intereses += 1
+            
+
+        # Devolver el puntaje normalizado
+        return score / total_intereses if total_intereses > 0 else 1.0
+
+    # Calcula las puntuaciones normalizadas de las Universidades entre los Participantes
     def evaluar_Universidad(self, team) -> float:
         if len(team) == 1:
             return 1.0
@@ -173,6 +204,66 @@ class TeamFormation:
 
         return score/len(team)
 
+    # Calcula las puntuaciones normalizadas de las preferencias entre los participantes
+    def evaluar_Preferencia(self, team) -> float:
+        score = 0
+
+        if len(team) == 1:
+            return 1.0
+    
+        normalizar = 0
+
+        for i in range(len(team)):
+            integrante1 = team[i]
+            for j in range(i + 1, len(team)):
+                integrante2 = team[j]
+
+                preferencia1 = self.Participantes.loc[self.Participantes['ID'] == integrante1['ID'], 'Preferred Role'].values[0]
+                preferencia2 = self.Participantes.loc[self.Participantes['ID'] == integrante2['ID'], 'Preferred Role'].values[0]
+
+                if preferencia1 not in INVALID_PREFERENCIA and preferencia2 not in INVALID_PREFERENCIA:
+                    if preferencia1 != preferencia2:
+                        score += 1
+                    normalizar += 1
+
+        return score / normalizar if normalizar > 0 else 1.0
+
+
+    # Evalua la similitud entre las experiencias cuanto mas diferentes entre ellas son mejor
+    def evaluar_Experiencia(self, team) -> float:
+        score = 0
+
+        if len(team) == 1:
+            return 1.0
+    
+        normalizar = 0
+
+        for i in range(len(team)):
+            integrante1 = team[i]
+            for j in range(i + 1, len(team)):
+                integrante2 = team[j]
+
+                experiencia1 = self.Participantes.loc[self.Participantes['ID'] == integrante1['ID'], 'Experience Level'].values[0]
+                experiencia2 = self.Participantes.loc[self.Participantes['ID'] == integrante2['ID'], 'Experience Level'].values[0]
+
+                if experiencia1 != experiencia2:
+                    score += 1
+            
+                normalizar += 1
+
+        return score/normalizar if normalizar > 0 else 1.0
+
+    def evaluar_Hackathons(self, team) -> float:
+        score = 0
+
+        max_hackathons = self.Participantes['Hackathons Done'].max()
+    
+        for integrante in team:
+            hackathons = self.Participantes.loc[self.Particiantes['ID'] == integrante['ID'], 'Hackathons Done'].values[0]
+            score += hackathons
+
+        return score/ (max_hackathons * len(team))
+
     # Calcula la compatibilidad que tiene los integrates del equipo entre si sin tener encuenta el 
     # usuario objetivo
     def evaluarCompatibilidadEquipo(self, team) -> float:
@@ -181,6 +272,10 @@ class TeamFormation:
         score += self.evaluar_Edad(team) * self.Ponderaciones[0]
         score += self.evaluar_AñoEscolar(team) * self.Ponderaciones[1]
         score += self.evaluar_Universidad(team) * self.Ponderaciones[2]
+        score += self.evaluar_Intereses(team) * self.Ponderaciones[3]
+        score += self.evaluar_Preferencia(team) * self.Ponderaciones[4]
+        score += self.evaluar_Experiencia(team) * self.Ponderaciones[5]
+        score += self.evaluar_Hackathons(team) * self.Ponderaciones[6]
 
         return score
 
