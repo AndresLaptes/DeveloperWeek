025ab99b329d796a081321fce7799241cec2d388
Rubén Palà Vacas <rubenyadriaya@gmail.com>
2025-01-30 12:09:02 +0100

    Fixed header centering.

--- frontend/src/components/ProfileMenu.jsx
+++ frontend/src/components/ProfileMenu.jsx
@@ -30,7 +30,7 @@ const ProfileMenu = ({children, initialIsLogged}) => {
 
 
     return (
-         <div>
+         <div ref={dropdownRef}>
             <div className="profile-container"> 
                 <img 
                     src= {isLogged ? userPhoto : defaultUser}
@@ -39,12 +39,12 @@ const ProfileMenu = ({children, initialIsLogged}) => {
                     onClick={() => setProfileDropdown(!profileDropdown)}
                 />
             </div>
-            <div className={`profile-dropdown ${profileDropdown ? 'active' : ''}`} ref={dropdownRef}>
+            <div className={`profile-dropdown ${profileDropdown ? 'active' : ''}`}>
                 
                 <div className={`profile-dropdown-arrow ${profileDropdown ? 'active' : ''}`}></div>
-                    {
-                        isLogged &&
-                        <ul className={`profile-menu-list $ logged`}>
+                {
+                    isLogged &&
+                    <ul className={`profile-menu-list $ logged`}>
                         <li className="menu-item">
                             <Link to="/Profile">Profile</Link>
                         </li>
@@ -52,23 +52,20 @@ const ProfileMenu = ({children, initialIsLogged}) => {
                             <Link to="/Settings">Settings</Link>
                         </li>
                         <li className="menu-item" onClick={() => setIsLogged(!isLogged)}>Logout</li>
-                        </ul>
-                    }
-                    
-                    {
-                        !isLogged &&
-                        <ul className={`profile-menu-list $ 'unlogged'`}>
-                            <li className="menu-item">
-                                <Link to="/Login">Log In</Link>
-                            </li>
-                            <li className="menu-item">
-                                <Link to="/Signup">Sign Up</Link>
-                            </li>
-                            <li className="menu-item" onClick={() => setIsLogged(!isLogged)}>Click to simulate login</li>
-                        </ul>
-                    }
-                    
-                
+                    </ul>
+                }
+                {
+                    !isLogged &&
+                    <ul className={`profile-menu-list $ 'unlogged'`}>
+                        <li className="menu-item">
+                            <Link to="/Login">Log In</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/Signup">Sign Up</Link>
+                        </li>
+                        <li className="menu-item" onClick={() => setIsLogged(!isLogged)}>Click to simulate login</li>
+                    </ul>
+                }   
             </div>
          </div>
     )
